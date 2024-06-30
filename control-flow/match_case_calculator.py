173115c633b8006000777a058423378e4c5b7cdf
moguen <smailelarroum@gmail.com>
2024-06-30 15:47:51 +0100

    Add match_case_calculator.py script

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,16 +3,15 @@ def get_number(prompt):
         try:
             return float(input(prompt))
         except ValueError:
-            print("Invalid input. Please enter a valid number.")
+            print("Invalid input. please enter a valid number.")
 def main():
-    num1 = get_number("Enter the first number : ")
+    num1 = get_number("Enter the first number: ")
     num2 = get_number("Enter the second number: ")
-
+    
     operation = input("choose the operation (+, -, *, /): ").strip()
-
     match operation:
         case"+":
-            result =num1 + num2
+            result = num1 + num2
             print(f"the result is {result}.")
         case"-":
             result = num1 - num2
@@ -21,13 +20,12 @@ def main():
             result = num1 * num2
             print(f"the result is {result}.")
         case"/":
-            if num2 != 0:
-                result = num1/num2
+            if num2 !=0:
+                result = num1 / num2
                 print(f"the result is {result}.")
             else:
-                print("cannot devide by 0")
+                print("cannot divide by zero.")
         case _:
-            print("Invalid operation. Please choose from (+, -, *, /).")
+            print("invalid operation., please choose from (+, -, *, /).")
 if __name__=="__main__":
     main()
-
