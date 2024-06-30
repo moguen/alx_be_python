9fde407e8944e0029aa74d0ca767c38b3d7c7b33
moguen <smailelarroum@gmail.com>
2024-06-30 18:56:19 +0100

    corrected

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,31 +1,23 @@
-def get_number(prompt):
-    while True:
-        try:
-            return float(input(prompt))
-        except ValueError:
-            print("Invalid input. please enter a valid number.")
-def main():
-    num1 = get_number("Enter the first number: ")
-    num2 = get_number("Enter the second number: ")
+def calculator():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    operation = input("choose the opration (+, *, -, /): ")
     
-    operation = input("choose the operation (+, -, *, /): ").strip()
     match operation:
-        case"+":
-            result = num1 + num2
-            print(f"the result is {result}.")
-        case"-":
-            result = num1 - num2
-            print(f"the result is {result}.")
-        case"*":
-            result = num1 * num2
-            print(f"the result is {result}.")
-        case"/":
-            if num2 !=0:
-                result = num1 / num2
-                print(f"the result is {result}.")
-            else:
-                print("cannot divide by zero.")
-        case _:
-            print("invalid operation., please choose from (+, -, *, /).")
-if __name__=="__main__":
-    main()
+      case"+":
+       result = num1 + num2
+       print (f"the result is {result}.")
+      case"-":
+       result = num1 - num2
+       print(f"the result is{result}.")
+      case"*":
+       result = num1 * num2
+       print(f"the result is {result}.")
+      case"/":
+       if num2 ==0:
+        print("cannot devide by zero")
+       else:
+        result = num1 / num2
+        print(f"the result is {result}.")
+calculator()
+
